7055ddf4e4c34f32db439d35deb0407006b3c2ba
Aniket-bansal <akbaisla29041998@gmail.com>
2022-10-07 17:12:30 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import {Box} from '@chakra-ui/react'
 import MainRoutes from './Routes/MainRoutes';
+import Navbar from './Components/Navbar';
 function App() {
   return (
-    <Box className="App">
+    <>
+    <Box marginBottom={"100px"} height="120px"><Navbar/></Box>
       <MainRoutes /> 
-    </Box>
+    </>
   );
 }
 
